9e34e9b28fdedc676b8ec683b46a6e9fe8029adc
SOL DIANA IZRAELEWICZ <48796333@ort>
2024-05-16 16:02:18 -0300

    Punto 2 y 3

--- Program.cs
+++ Program.cs
@@ -4,33 +4,119 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+        
+        int op, clientes = 0;
 
-        int op;
-        Console.WriteLine("1. Nueva Inscripción");
-        Console.WriteLine("2. Obtener Estadísticas del Evento");
-        Console.WriteLine("3. Buscar Cliente");
-        Console.WriteLine("4. Cambiar Entrada de un Cliente");
-        Console.WriteLine("5. Salir");
+
 
         op = ingresarOpcion("Ingrese la opcion deseada", 1, 5);
-
         do
         {
             switch (op)
             {
-                case 1: 
+                case 1:
+                
+                    ingresarDatosClientes();
+                    clientes++;
+
+                break;
+
+                case 2:
+                    List<string> ResultadosEstadistica = Ticketera.MostrarEstadisticas();
+                    foreach (String item in ResultadosEstadistica) Console.WriteLine(item);
                     
+                break;
+
+                case 3:
+                    int id = ingresarInt("Ingrese el ID de el cliente que quiere buscar:", 1, clientes+1);
+                    Cliente obj = new Cliente ();
+                    obj = Ticketera.buscarCliente(id);
+                    
+                    Console.WriteLine("El dni del cliente es: " + obj.DNI);
+                    Console.WriteLine("El nombre del cliente es: " + obj.Nombre);
+                    Console.WriteLine("El apellido del cliente es: " + obj.Apellido);
+                    Console.WriteLine("La fecha en la que el cliente se inscribió fue: " + obj.fechaInscripcion);
+                    Console.WriteLine("El tipo de entrada que compro el cliente es: " + obj.tipoEntrada);
+                    Console.WriteLine("La cantidad de tickets que compró fue: " + obj.cantidad);
+
+                break;
+
+                case 4:
+
                 break;
             
             }
+            op = ingresarOpcion("Ingrese la opcion deseada", 1, 5);
+
         } while (op != 5);
         
 
     }
 
+
+    public static void ingresarDatosClientes()
+    {
+        int valor = int.MaxValue;
+        
+        int dni = ingresarInt("Ingrese su DNI", 0, valor);
+        string nombre = ingresarString("Ingrese su nombre");
+        string apellido = ingresarString("Ingrese su apellido");
+        DateTime fecha = ingresarFecha("Ingrese su fecha de inscripción");
+        int tipoEntrada = ingresarInt("Ingrese su tipo de entrada (1, 2, 3, 4)", 1, 4);
+        int cant = ingresarInt("Ingresa la cantidad de entradas que desea", 1, valor);
+        
+        Cliente UnCLiente = new Cliente(dni, nombre, apellido, fecha,tipoEntrada, cant);
+        int NroTicket = Ticketera.AgregarCliente(UnCLiente);
+        Console.WriteLine("Compraste entradas. Tu numero de ticket de compra es " + NroTicket);
+
+    }
+
+    public static DateTime ingresarFecha(string msj)
+    {
+        DateTime fecha;
+        do
+        {
+             Console.WriteLine(msj);
+             fecha = DateTime.Parse(Console.ReadLine());
+             if (fecha > DateTime.Today) Console.WriteLine("ERROR! Ingrese nuevamente");
+        } while (fecha > DateTime.Today);
+        return fecha; 
+    }
+
+    public static string ingresarString(string msj)
+    {
+        string caracteres;
+        do
+        {
+            Console.WriteLine(msj);
+            caracteres = Console.ReadLine();
+            if (caracteres == string.Empty) Console.WriteLine("ERROR! Ingrese nuevamente");
+        } while (caracteres == string.Empty);
+        return caracteres;
+    }
+
+    public static int ingresarInt(string msj, int min, int max)
+    {
+        int num;
+        do
+        {
+            Console.WriteLine(msj);
+            num = int.Parse(Console.ReadLine()); // poner TRY PARSEEEEEEEEEEEEEEEEEEEE
+            if (num < min || num > max) Console.WriteLine("ERROR! Ingrese nuevamente");
+        } while (num < min || num > max);
+        return num;
+    }
     public static int ingresarOpcion(string msj, int min, int max)
     {
         int op;
+
+        
+        Console.WriteLine("1. Nueva Inscripción");
+        Console.WriteLine("2. Obtener Estadísticas del Evento");
+        Console.WriteLine("3. Buscar Cliente");
+        Console.WriteLine("4. Cambiar Entrada de un Cliente");
+        Console.WriteLine("5. Salir");
 
         do
         {
--- Ticketera.cs
+++ Ticketera.cs
@@ -1,9 +1,81 @@
 static class Ticketera
 {
     private static Dictionary<int, Cliente> dicCliente = new Dictionary<int, Cliente>();
-    private static int ultimoIDEntrada = 1;
+    private static int ultimoIDEntrada = 0;
     
+    public static int AgregarCliente(Cliente UnCliente){
+        ultimoIDEntrada++;
+        dicCliente.Add(ultimoIDEntrada,UnCliente);
+        return ultimoIDEntrada;
+       
+    }
+
+    public static Cliente buscarCliente(int id)
+    {
+        return dicCliente[id];
+    }
+
+   
+
+    public static List<string> MostrarEstadisticas(){
+        
+        List<string> Lista = new List<string>();
+      
+        //Calcular
+        int[] PreciosTipo = new int[5]{0,45000,60000,30000,100000};
+        int total = 0;
+        int[] cantPorEntrada = new int[5];
+        int[] cantPorTicket = new int[5];
+        int cantTotalEntradas = 0;
+        foreach (Cliente item in dicCliente.Values)
+        {
+            
+            cantTotalEntradas = cantTotalEntradas + item.cantidad;
+            cantPorEntrada[item.tipoEntrada] ++;
+            cantPorTicket[item.tipoEntrada] += item.cantidad;
+
+            total += item.cantidad * PreciosTipo[item.tipoEntrada];
+
+            
+        }
+        Lista.Add("La cantidad de Clientes inscriptos es: " + dicCliente.Count.ToString());
+        Lista.Add("La cantidad de personas que compraron la entrada 1 son: " + cantPorEntrada[1] + "  La cantidad de personas que compraron la entrada 2 son: " + cantPorEntrada[2] + "  La cantidad de personas que compraron la entrada 3 son: " + cantPorEntrada[3] + "  La cantidad de personas que compraron la entrada 4 son: " + cantPorEntrada[4]);        
+        
+         //ver porque esta mal el porcentaje
+        Lista.Add("El porcentaje de la cantidad de tipo 1 es de: " + ((double)(cantPorTicket[1] / cantTotalEntradas *100)) + "  El porcentaje de la cantidad de tipo 2 es de: " + ((double)(cantPorTicket[2] / cantTotalEntradas *100)) + 
+                  "El porcentaje de la cantidad de tipo 3 es de: " + ((double)(cantPorTicket[3] / cantTotalEntradas *100)) + "  El porcentaje de la cantidad de tipo 4 es de: " + ((double)(cantPorTicket[4] / cantTotalEntradas *100)));
+        
+        Lista.Add("La recaudación total de las entradas de tipo 1 es de: $" + (cantPorTicket[1] * PreciosTipo[1]) + "  La recaudación total de las entradas de tipo 2 es de: $" + (cantPorTicket[2] * PreciosTipo[2]) + 
+                  "La recaudación total de las entradas de tipo 3 es de: $" + (cantPorTicket[3] * PreciosTipo[3]) + "  La recaudación total de las entradas de tipo 4 es de: $" + (cantPorTicket[4] * PreciosTipo[4]));
+        Lista.Add("La recaudación total es de: $" + total);
+
+        return  Lista;
+    }
 
 
+     private static void acumularAbono(Cliente UnCliente, int[] cantPorEntrada, int t1, int t2, int t3, int t4, int total)
+    { 
+        if (UnCliente.tipoEntrada == 1)
+        {
+            total += t1 * UnCliente.cantidad;
+            cantPorEntrada[1] ++;
+        }
+        else if (UnCliente.tipoEntrada == 2)
+        {
+            total += t2 * UnCliente.cantidad;
+            cantPorEntrada[2] ++;
+        }
+        else if (UnCliente.tipoEntrada == 3)
+        {
+            total += t3 * UnCliente.cantidad;
+            cantPorEntrada[3] ++;
+        }
+        
+        else if (UnCliente.tipoEntrada == 4)
+        {
+            total += t4 * UnCliente.cantidad;
+            cantPorEntrada[4] ++;
+        }
+    }
 
 }
